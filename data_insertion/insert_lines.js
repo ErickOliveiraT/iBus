ee5e15e07790416f152c7546a2febcd6f9e2e6be
Érick de Oliveira Teixeira <38773400+ErickOliveiraT@users.noreply.github.com>
2020-10-19 23:00:30 -0300

    insert lines csv

--- data_insertion/insert_lines.js
+++ data_insertion/insert_lines.js
@@ -10,11 +10,17 @@ async function run() {
     let lines = csvwp.getLines('./data/lines_csv.csv', options);
     
     for (let i = 1; i < lines.length-1; i++) {
-        let id = lines[i].split(';')[0];
+        let line = lines[i].split(';')[0];
         let agency = lines[i].split(';')[1];
         let name = lines[i].split(';')[2].split(' - ')[1];
-        let data = {id, agency, name};
-        console.log(data);
+        let data = {line, agency, name};
+        let options = {
+            method: 'post',
+            url: 'localhost:4000/lines',
+            data: JSON.stringify(data)
+        }
+        //console.log(`('${line}','${agency}','${name}'),`)
+        axios(options);
     }
 }
 
